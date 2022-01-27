7010fd19f0be5c18de39f483dfdd6b7a881cab2d
Katona <annainterior14@gmail.com>
2022-01-27 18:34:09 +0300

    add default healthbar and pain dealer to show damage taken

--- UnknownSteampunk/Source/UnknownSteampunk/UnknownSteampunkGameMode.cpp
+++ UnknownSteampunk/Source/UnknownSteampunk/UnknownSteampunkGameMode.cpp
@@ -2,9 +2,14 @@
 
 #include "UnknownSteampunkGameMode.h"
 #include "UnknownSteampunkCharacter.h"
+#include "UObject/ConstructorHelpers.h"
 
 AUnknownSteampunkGameMode::AUnknownSteampunkGameMode()
 {
-	// Set default pawn class to our character
-	DefaultPawnClass = AUnknownSteampunkCharacter::StaticClass();	
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/SideScrollerCPP/Blueprints/SideScrollerCharacter"));
+	if (PlayerPawnBPClass.Class != nullptr)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
 }
